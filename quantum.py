2f218ca1090217a57cf25f289768ae9d79fadf26
Julian Gomez <juliantesla13@gmail.com>
2018-11-30 11:46:21 -0500

    Add files via upload

--- quantum.py
+++ quantum.py
@@ -1,59 +1,67 @@
+#!/usr/bin/python
+
+__author__ ="Julian Andres Gomez"
+__copyright__ = "Copyright 2018, Qbeta"
+__credits__ = "Qbeta"
+__license__ = "Apache License 2.0"
+__version__ = "1.0.1"
+__maintainer__ = "Julian Andres Gomez"
+__email__ = "juliantesla13@gmail.com"
+__status__ = "Production"
+
 import numpy as np
 import math as mt
 import cmath as cmt
  
-#crear los dos estados cuantico en la funcion qbit
-
 def qbit():
+    """create the two quantum states in the qbit function """
     Zero = np.array([[1.0],  [0.0]])
     One = np.array([[0.0],   [1.0]])
     return Zero, One
 
-# normalizar los estados 
 def NormState(state): 
+    """normalize the states """
     return  state / np.linalg.norm(state)
-
-    
-    
-# compuerta de hardamard    
+   
 def Hadamard():
+    """gate of hardamard   """
     return 1./np.sqrt(2) * np.array([[1, 1], [1, -1]])
 
-#crear un nuevo estado 
 def NewState(p, q):
+    """create a new state  """
     return np.dot(p, q)
 
-# Calcular un producto Kronecker sobre un número variable de entradas
 def NKron(*args):
+  """ Calculate a Kronecker product on a variable number of entries """
   result = np.array([[1.0]])
   for op in args:
     result =np.kron(result, op)
   return result
 
-# Calcular un producto Kronecker sobre un par de variables de entradas
 def kron(a, b):
+    """ Calculate a Kronecker product on a pair of input variables """
     return np.kron(a, b)  
 
-#compuerta X
 def gatex():
+    """gate X"""
     return  np.array([[0,1], [1,0]])
 
-#compuerta P0
 def gatep0():
+    """gate P0 """
     Zero = np.array([[1.0],  [0.0]])
     return np.dot(Zero, Zero.T)
 
-#compuerta P1   
 def gatep1():
+    """gate P1  """
     One = np.array([[0.0],  [1.0]])
     return  np.dot(One, One.T)
 
-#Puertas de desplazamiento de fase
 def gateR(theta):
+    """ Phase shift doors"""
     return  np.array([[1,0], [0, cmt.exp( theta*1j )]])
 
-# medir el estado cuantico 
 def medirstate(qbits):
+    """ measure the quantum state """
     nqbits =NormState(qbits)
     qbits = np.dot(nqbits, nqbits.T)
     P0= gatep0()
@@ -70,23 +78,20 @@ def medirstate(qbits):
     
     return Prob0.real
 
-#cuerpo del progrma
+if  __name__=='__main__':
 
-def main():
-    #creacion de los qbits
+    #creation of the qbits
     Zero, One =qbit()
-    #crea la matriz identidad
+    #create the identity matrix
     Id = np.eye(2)
     x1=Zero
-    #defino un estado cuantico 
+    #I define a quantum state
     Hx1=NewState(Hadamard(),x1)
-    #medicion del estado cuantico 
+    #quantum state measurement
     s=medirstate (Hx1)
     print (s)
-    return 0
 
-# funcion de inicio
-main()
+
 
 
 
